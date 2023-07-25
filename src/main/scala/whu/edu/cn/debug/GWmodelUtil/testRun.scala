c0e1e2d4289845f1c03ee76a0e8ccb47a01e394b
OuGuangyu <g_ruoyu@foxmail.com>
2023-07-25 16:26:51 +0800

    executed write shpfile, need to create function

--- src/main/scala/whu/edu/cn/debug/GWmodelUtil/testRun.scala
+++ src/main/scala/whu/edu/cn/debug/GWmodelUtil/testRun.scala
@@ -1,11 +1,15 @@
 package whu.edu.cn.debug.GWmodelUtil
 
+import geotrellis.vector.MultiPolygon
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.rdd.RDD
-import org.locationtech.jts.geom.{Coordinate, Geometry}
+import org.locationtech.jts.geom.{Coordinate, Geometry, LineString}
 import whu.edu.cn.oge.Feature
-import whu.edu.cn.util.ShapeFileUtil
+import whu.edu.cn.util.ShapeFileUtil._
+
+import java.awt.Polygon
 import scala.collection.immutable.List
+import scala.collection.mutable
 import scala.collection.mutable.{ArrayBuffer, Map}
 import scala.math.{abs, max, min, pow, sqrt}
 import scala.reflect.ClassTag
@@ -16,6 +20,7 @@ import org.apache.spark.sql.{DataFrame, Row}
 import org.apache.spark.mllib.stat.Statistics
 import breeze.numerics._
 import breeze.linalg.{Vector, DenseVector, Matrix , DenseMatrix}
+import scala.collection.JavaConverters._
 
 import whu.edu.cn.debug.GWmodelUtil.GWMdistance._
 import whu.edu.cn.debug.GWmodelUtil.GWMspatialweight._
@@ -23,26 +28,49 @@ import whu.edu.cn.debug.GWmodelUtil.sp_autocorrelation._
 
 object testRun {
   def main(args: Array[String]): Unit = {
-    //    val time1: Long = System.currentTimeMillis()
-    //    println(time1)]
+
     val conf: SparkConf = new SparkConf().setMaster("local[8]").setAppName("query")
     val sc = new SparkContext(conf)
 
     val shpPath: String = "testdata\\MississippiHR.shp" //我直接把testdata放到了工程目录下面，需要测试的时候直接使用即可
-    val shpfile = ShapeFileUtil.readShp(sc,shpPath,ShapeFileUtil.DEF_ENCODE)//或者直接utf-8
-    println(getGeometryType(shpfile))
-
-    val globali=globalMoranI(shpfile,"HR60")
-    println(s"global Moran's I is: $globali")
-
+    val shpfile = readShp(sc,shpPath,DEF_ENCODE)//或者直接utf-8
+    val geom=println(getGeometryType(shpfile))
     println("-------------")
     val locali=localMoranI(shpfile,"HR60")
     println("-----------local moran's I--------------")
-    locali._1.foreach(println)
-    println("-------------expectation------------")
-    locali._2.foreach(println)
+//    locali._1.foreach(println)
+
+    val shp = shpfile.map(t => t._2._2).collect()
+    for (i <- 0 until locali._1.length) {
+      shp(i) += ("locali" -> locali._1(i))
+    }
+//    println("-------shp-----------")
+//    shp.foreach(println)
+
+    val outdata = shpfile.collect().zipWithIndex
+    outdata.map(t => {
+      t._1._2._2 += ("locali"->locali._1(t._2))
+    })
+    val result=sc.makeRDD(outdata.map(t=>t._1))
+    println("-------------result------------------")
+    result.collect().foreach(println)
+
+    createShp("testdata\\MississippiMorani.shp","utf-8",classOf[MultiPolygon],result.map(t => {
+      t._2._2 + (DEF_GEOM_KEY -> t._2._1)
+    }).collect().map(_.asJava).toList.asJava)
+    println("成功落地shp")
+
+//    var data=shpfile.collect()
+//    data.map(shpt=>{
+////      shpt._2._2 += ("locali",0.0)
+//      shpt._2._2=shp.foreach(t=>t)
+//    })
+//    println("-------------re------------------")
+//    data.map(t=>t._2._2).foreach(println)
 
 
+//    val globali = globalMoranI(shpfile, "HR60")
+//    println(s"global Moran's I is: $globali")
 
     //    val geom=getGeometry(shpfile)
     //    val nb=getNeighborBool(geom)
@@ -58,6 +86,15 @@ object testRun {
 //    val time3: Long = System.currentTimeMillis()
 //    println(time3-time2,time2-time1)
 
+  }
+
+  def makenewdata(data: Array[Map[String, Any]], geom: Array[Geometry])={
+        for (i <- 0 until geom.length) {
+          data.map(t=>{
+            (geom(i),t)
+          })
+
+    }
   }
 
   def printArrArr[T: ClassTag](arrarr: Array[Array[T]]) = {
