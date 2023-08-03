264046dfabc0de95043bd9697e7174af6b7f1b3f
whm0305 <492900841@qq.com>
2023-08-03 17:23:38 +0800

    add 平均最近邻算子

--- src/main/scala/whu/edu/cn/debug/GWmodelUtil/AveNearestNeighbor.scala
+++ src/main/scala/whu/edu/cn/debug/GWmodelUtil/AveNearestNeighbor.scala
@@ -34,11 +34,6 @@ object AveNearestNeighbor {
 
   }
 
-  //满足按距离排序后除0以外的最小距离
-  def dismin(Arr1: Array[Double]) : Double = {
-      Arr1.sorted.filter(_>0).apply(0)
-  }
-
   //要素的最小外接矩阵面积
   def ExRectangularArea(testshp: RDD[(String, (Geometry, Map[String, Any]))]) : Double = {
     val coorxy : Array[(Double, Double)] = getCoorXY(testshp: RDD[(String, (Geometry, Map[String, Any]))])
@@ -53,14 +48,16 @@ object AveNearestNeighbor {
   //平均最近邻指数算子
   def AveNearestNeighbor(sc: SparkContext,testshp: RDD[(String, (Geometry, Map[String, Any]))]): Unit={
     val DisArray = getRDDDistRDD(sc,testshp)  //getRDDDistRDD函数构造RDD的距离矩阵
-    //DisArray.collect().map(t => println(dismin(t)))
-    val DisSum = DisArray.collect().map(t => dismin(t)).sum  //t指每一行，取出RDD矩阵每行最小距离，求和
+    //DisArray.collect().map(t => println(t.filter(_>0).min))
+    val DisSum = DisArray.collect().map(t => t.sorted.apply(1)).sum  //t指每一行，取出RDD矩阵每行最小距离，求和 欧式距离矩阵
     val RDDsize =  DisArray.collect().size  //RDD要素个数，同length
     val A  = ExRectangularArea(testshp)
-    val Do = DisSum/RDDsize  //观测平均距离（Observed Mean Distance）
+    val Do = DisSum/RDDsize  //平均观测距离（Observed Mean Distance）
     val De = 0.5/(sqrt(RDDsize/A))  //预期平均距离 A为研究区域面积，要素的外接矩形
     val ANN = Do/De  //平均最近邻指数  ANN>1离散  ANN<1聚集
-    println(ANN)
+    val SE = 0.26136/(sqrt(pow(RDDsize, 2)/ A))
+    val z = (Do - De)/SE
+    println(ANN,z)
   }
 
 }
