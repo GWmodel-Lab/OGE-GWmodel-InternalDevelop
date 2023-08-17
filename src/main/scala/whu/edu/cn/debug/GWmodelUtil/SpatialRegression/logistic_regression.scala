365096bd9a6d5018ef2660565e02ba0cdb626b75
OuGuangyu <g_ruoyu@foxmail.com>
2023-08-17 12:16:12 +0800

    add logistic and poisson regression object, not complete

---
+++ src/main/scala/whu/edu/cn/debug/GWmodelUtil/SpatialRegression/logistic_regression.scala
@@ -0,0 +1,13 @@
+package whu.edu.cn.debug.GWmodelUtil.SpatialRegression
+
+import breeze.linalg.DenseVector
+
+object logistic_regression {
+
+  def logisticRegression(x: Array[DenseVector[Double]], y: DenseVector[Double]): Unit = {
+
+
+  }
+
+
+}
