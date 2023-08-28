20a1159bfefa67c1f25a3be207dc5a4ec0cc8528
mibugii <cheska0819@163.com>
2023-08-28 19:11:23 +0800

    modify descriptive_statistics: complete annotation modify data filepath delete KDE

--- src/main/scala/whu/edu/cn/debug/GWmodelUtil/testRun.scala
+++ src/main/scala/whu/edu/cn/debug/GWmodelUtil/testRun.scala
@@ -1,146 +1,146 @@
-package whu.edu.cn.debug.GWmodelUtil
-
-import org.apache.spark.{SparkConf, SparkContext}
-import org.apache.spark.rdd.RDD
-import org.locationtech.jts.geom.{Coordinate, Geometry}
-import whu.edu.cn.oge.Feature
-import whu.edu.cn.util.ShapeFileUtil
-import scala.collection.immutable.List
-import scala.collection.mutable.{ArrayBuffer, Map}
-import scala.math.{abs, max, min, pow, sqrt}
-import scala.reflect.ClassTag
-//import org.apache.spark.mllib.linalg._
-import org.apache.spark.ml.feature.VectorAssembler
-import org.apache.spark.ml.stat.Correlation
-import org.apache.spark.sql.{DataFrame, Row}
-import org.apache.spark.mllib.stat.Statistics
-import breeze.numerics._
-import breeze.linalg.{Vector, DenseVector, Matrix , DenseMatrix}
-
-import whu.edu.cn.debug.GWmodelUtil.Utils.GWMdistance._
-import whu.edu.cn.debug.GWmodelUtil.Utils.GWMspatialweight._
-
-
-object testRun {
-  def main(args: Array[String]): Unit = {
-    //    val time1: Long = System.currentTimeMillis()
-    //    println(time1)]
-    val conf: SparkConf = new SparkConf().setMaster("local[8]").setAppName("query")
-    val sc = new SparkContext(conf)
-
-    val shpPath: String = "testdata\\MississippiHR.shp" //我直接把testdata放到了工程目录下面，需要测试的时候直接使用即可
-    val shpfile = ShapeFileUtil.readShp(sc,shpPath,ShapeFileUtil.DEF_ENCODE)//或者直接utf-8
-    println(getGeometryType(shpfile))
-
-
-//    val geom=getGeometry(shpfile)
-//    val nb=getNeighborBool(geom)
-//    val idx=boolNeighborIndex(nb).collect()
-//    printArrArr(idx)
-    val nb_weight=getNeighborWeight(shpfile)
-    nb_weight.collect().foreach(println)
-
-
-//    val time1: Long = System.currentTimeMillis()
-//    val rdddist=getRDDDistRDD(sc,shpfile)
-////    printArrArr(rdddist.collect())
-//    val time2: Long = System.currentTimeMillis()
-//    val rddweight=getSpatialweight(rdddist,50,"gaussian",true)
-//    rddweight.collect().foreach(println)
-//    val time3: Long = System.currentTimeMillis()
-//    println(time3-time2,time2-time1)
-
-//    val apoint=getCoorXY(shpfile)
-//    val aX=apoint.take(1)
-//    val arr0=arrayDist(aX,apoint)
-//    //    arr0.foreach(println)
-//    val dv: DenseVector[Double] = new DenseVector(arr0)
-////    arr0.sorted.take(20).foreach(println)
-//    val weight = getSpatialweightSingle(dv,10,"bisquare",true)
-//    weight.foreach(println)
-
-//    println("gaussianKernelFunction")
-//    val weight1 = GaussianKernelFunction(dv,bw)
-//    weight1.foreach(println)
-//    println("exponentialKernelFunction")
-//    val weight2 = ExponentialKernelFunction(dv, bw)
-//    weight2.foreach(println)
-//    println("BisquareKernelFunction")
-//    val weight3 = bisquareKernelFunction(dv, bw)
-//    weight3.foreach(println)
-//    println("TricubeKernelFunction")
-//    val weight4 = tricubeKernelFunction(dv, bw)
-//    weight4.foreach(println)
-//    println("BoxcarKernelFunction")
-//    val weight5 = boxcarKernelFunction(dv, bw)
-//    weight5.foreach(println)
-
-//    val arr4=apoint.take(4)
-//    val arr3=apoint.take(3)
-//    println("arr4")
-//    arr4.foreach(println)
-//    println("arr3")
-//    arr3.foreach(println)
-//    val arrd=arrayDist(arr4,arr3)
-//    println("dis")
-//    arrd.foreach(println)
-//    val dmat1= new DenseMatrix(arr3.length, arr4.length, arrd)
-//    println(dmat1.transpose)
-//    val dmat2 = getArrDistDmat(arr4,arr3)
-//    println(dmat2)
-
-    //    val testshp=shpfile.map(t=>{
-    //      val geom=t._2._1
-    //      val prop=t._2._2("Avg_AQI")
-    //      (geom,prop)
-    //    })
-
-  }
-
-  def printArrArr[T: ClassTag](arrarr: Array[Array[T]]) = {
-    val arrvec=arrarr.map(t=>t.toVector)
-    arrvec.foreach(println)
-  }
-
-  def testcorr(testshp: RDD[(String, (Geometry, Map[String, Any]))]) : Unit={
-    val time0: Long = System.currentTimeMillis()
-    val list1:List[Any] = Feature.get(testshp,"PURCHASE")//FLOORSZ,PROF
-    val list2:List[Any] = Feature.getNumber(testshp,"FLOORSZ")
-//    println(list1)
-//    println(list2)
-    val lst1:List[Double] = list1.collect({ case (i: String) => (i.toDouble) })
-//    //val lst2:List[Double]=list2.collect({ case (i: String) => (i.toDouble) })
-    val lst2:List[Double] = list2.asInstanceOf[List[Double]]
-    val corr1 = corr2list(lst1,lst2)
-    val listtimeused= System.currentTimeMillis()-time0
-    println(s"time used is: $listtimeused")
-
-    val coor2 = corr2ml(testshp,"PURCHASE","FLOORSZ")
-
-  }
-
-  def corr2list(lst1: List[Double], lst2: List[Double]): Double = {
-    val sum1 = lst1.sum
-    val sum2 = lst2.sum
-    val square_sum1 = lst1.map(x => x * x).sum
-    val square_sum2 = lst2.map(x => x * x).sum
-    val zlst = lst1.zip(lst2)
-    val product = zlst.map(x => x._1 * x._2).sum
-    val numerator = product - (sum1 * sum2 / lst1.length)
-    val dominator = pow((square_sum1 - pow(sum1, 2) / lst1.length) * (square_sum2 - pow(sum2, 2) / lst2.length), 0.5)
-    val correlation = numerator / (dominator * 1.0)
-    println(s"Correlation is: $correlation")
-    correlation
-  }
-
-  def corr2ml(feat: RDD[(String, (Geometry, Map[String, Any]))], property1: String, property2: String): Double = {
-    val time0: Long = System.currentTimeMillis()
-    val aX: RDD[Double] = feat.map(t => t._2._2(property1).asInstanceOf[String].toDouble)
-    val aY: RDD[Double] = feat.map(t => t._2._2(property2).asInstanceOf[String].toDouble)
-    val correlation: Double = Statistics.corr(aX, aY, "pearson") //"spearman"
-    val timeused: Long = System.currentTimeMillis() - time0
-    println(s"Correlation is: $correlation")
-    println(s"time used is: $timeused")
-    correlation
-  }
-}
+//package whu.edu.cn.debug.GWmodelUtil
+//
+//import org.apache.spark.{SparkConf, SparkContext}
+//import org.apache.spark.rdd.RDD
+//import org.locationtech.jts.geom.{Coordinate, Geometry}
+//import whu.edu.cn.oge.Feature
+//import whu.edu.cn.util.ShapeFileUtil
+//import scala.collection.immutable.List
+//import scala.collection.mutable.{ArrayBuffer, Map}
+//import scala.math.{abs, max, min, pow, sqrt}
+//import scala.reflect.ClassTag
+////import org.apache.spark.mllib.linalg._
+//import org.apache.spark.ml.feature.VectorAssembler
+//import org.apache.spark.ml.stat.Correlation
+//import org.apache.spark.sql.{DataFrame, Row}
+//import org.apache.spark.mllib.stat.Statistics
+//import breeze.numerics._
+//import breeze.linalg.{Vector, DenseVector, Matrix , DenseMatrix}
+//
+//import whu.edu.cn.debug.GWmodelUtil.Utils.GWMdistance._
+//import whu.edu.cn.debug.GWmodelUtil.Utils.GWMspatialweight._
+//
+//
+//object testRun {
+//  def main(args: Array[String]): Unit = {
+//    //    val time1: Long = System.currentTimeMillis()
+//    //    println(time1)]
+//    val conf: SparkConf = new SparkConf().setMaster("local[8]").setAppName("query")
+//    val sc = new SparkContext(conf)
+//
+//    val shpPath: String = "testdata\\MississippiHR.shp" //我直接把testdata放到了工程目录下面，需要测试的时候直接使用即可
+//    val shpfile = ShapeFileUtil.readShp(sc,shpPath,ShapeFileUtil.DEF_ENCODE)//或者直接utf-8
+//    println(getGeometryType(shpfile))
+//
+//
+////    val geom=getGeometry(shpfile)
+////    val nb=getNeighborBool(geom)
+////    val idx=boolNeighborIndex(nb).collect()
+////    printArrArr(idx)
+//    val nb_weight=getNeighborWeight(shpfile)
+//    nb_weight.collect().foreach(println)
+//
+//
+////    val time1: Long = System.currentTimeMillis()
+////    val rdddist=getRDDDistRDD(sc,shpfile)
+//////    printArrArr(rdddist.collect())
+////    val time2: Long = System.currentTimeMillis()
+////    val rddweight=getSpatialweight(rdddist,50,"gaussian",true)
+////    rddweight.collect().foreach(println)
+////    val time3: Long = System.currentTimeMillis()
+////    println(time3-time2,time2-time1)
+//
+////    val apoint=getCoorXY(shpfile)
+////    val aX=apoint.take(1)
+////    val arr0=arrayDist(aX,apoint)
+////    //    arr0.foreach(println)
+////    val dv: DenseVector[Double] = new DenseVector(arr0)
+//////    arr0.sorted.take(20).foreach(println)
+////    val weight = getSpatialweightSingle(dv,10,"bisquare",true)
+////    weight.foreach(println)
+//
+////    println("gaussianKernelFunction")
+////    val weight1 = GaussianKernelFunction(dv,bw)
+////    weight1.foreach(println)
+////    println("exponentialKernelFunction")
+////    val weight2 = ExponentialKernelFunction(dv, bw)
+////    weight2.foreach(println)
+////    println("BisquareKernelFunction")
+////    val weight3 = bisquareKernelFunction(dv, bw)
+////    weight3.foreach(println)
+////    println("TricubeKernelFunction")
+////    val weight4 = tricubeKernelFunction(dv, bw)
+////    weight4.foreach(println)
+////    println("BoxcarKernelFunction")
+////    val weight5 = boxcarKernelFunction(dv, bw)
+////    weight5.foreach(println)
+//
+////    val arr4=apoint.take(4)
+////    val arr3=apoint.take(3)
+////    println("arr4")
+////    arr4.foreach(println)
+////    println("arr3")
+////    arr3.foreach(println)
+////    val arrd=arrayDist(arr4,arr3)
+////    println("dis")
+////    arrd.foreach(println)
+////    val dmat1= new DenseMatrix(arr3.length, arr4.length, arrd)
+////    println(dmat1.transpose)
+////    val dmat2 = getArrDistDmat(arr4,arr3)
+////    println(dmat2)
+//
+//    //    val testshp=shpfile.map(t=>{
+//    //      val geom=t._2._1
+//    //      val prop=t._2._2("Avg_AQI")
+//    //      (geom,prop)
+//    //    })
+//
+//  }
+//
+//  def printArrArr[T: ClassTag](arrarr: Array[Array[T]]) = {
+//    val arrvec=arrarr.map(t=>t.toVector)
+//    arrvec.foreach(println)
+//  }
+//
+//  def testcorr(testshp: RDD[(String, (Geometry, Map[String, Any]))]) : Unit={
+//    val time0: Long = System.currentTimeMillis()
+//    val list1:List[Any] = Feature.get(testshp,"PURCHASE")//FLOORSZ,PROF
+//    val list2:List[Any] = Feature.getNumber(testshp,"FLOORSZ")
+////    println(list1)
+////    println(list2)
+//    val lst1:List[Double] = list1.collect({ case (i: String) => (i.toDouble) })
+////    //val lst2:List[Double]=list2.collect({ case (i: String) => (i.toDouble) })
+//    val lst2:List[Double] = list2.asInstanceOf[List[Double]]
+//    val corr1 = corr2list(lst1,lst2)
+//    val listtimeused= System.currentTimeMillis()-time0
+//    println(s"time used is: $listtimeused")
+//
+//    val coor2 = corr2ml(testshp,"PURCHASE","FLOORSZ")
+//
+//  }
+//
+//  def corr2list(lst1: List[Double], lst2: List[Double]): Double = {
+//    val sum1 = lst1.sum
+//    val sum2 = lst2.sum
+//    val square_sum1 = lst1.map(x => x * x).sum
+//    val square_sum2 = lst2.map(x => x * x).sum
+//    val zlst = lst1.zip(lst2)
+//    val product = zlst.map(x => x._1 * x._2).sum
+//    val numerator = product - (sum1 * sum2 / lst1.length)
+//    val dominator = pow((square_sum1 - pow(sum1, 2) / lst1.length) * (square_sum2 - pow(sum2, 2) / lst2.length), 0.5)
+//    val correlation = numerator / (dominator * 1.0)
+//    println(s"Correlation is: $correlation")
+//    correlation
+//  }
+//
+//  def corr2ml(feat: RDD[(String, (Geometry, Map[String, Any]))], property1: String, property2: String): Double = {
+//    val time0: Long = System.currentTimeMillis()
+//    val aX: RDD[Double] = feat.map(t => t._2._2(property1).asInstanceOf[String].toDouble)
+//    val aY: RDD[Double] = feat.map(t => t._2._2(property2).asInstanceOf[String].toDouble)
+//    val correlation: Double = Statistics.corr(aX, aY, "pearson") //"spearman"
+//    val timeused: Long = System.currentTimeMillis() - time0
+//    println(s"Correlation is: $correlation")
+//    println(s"time used is: $timeused")
+//    correlation
+//  }
+//}
--- src/main/scala/whu/edu/cn/debug/GWmodelUtil/testrun_lqy.scala
+++ src/main/scala/whu/edu/cn/debug/GWmodelUtil/testrun_lqy.scala
@@ -9,55 +9,20 @@ import whu.edu.cn.util.ShapeFileUtil
 import scala.collection.immutable.List
 import scala.collection.mutable.Map
 
-
-//run kde
-//import geotrellis.raster.{ColorMap, ColorRamps, RasterExtent, Tile}
-//import geotrellis.raster.mapalgebra.focal.Kernel
-//import geotrellis.vector.{Extent, Feature, Point, PointFeature}
-//import geotrellis.vector._
-//import scala.util._
-//import geotrellis.raster._
-//import scala.util.Random
-
-object testrun_lqy{
-  def main(args: Array[String]):Unit={
+object testrun_lqy {
+  def main(args: Array[String]): Unit = {
     //读取数据
-    val shppath:String="testdata\\LNHP100.shp"//自己改路径
+    val shppath: String = "testdata\\LNHP100.shp"
+    //自己改路径
     val conf: SparkConf = new SparkConf().setMaster("local[8]").setAppName("query")
     val sc = new SparkContext(conf)
-    val shpfile : RDD[(String, (Geometry, Map[String, Any]))] = ShapeFileUtil.readShp(sc,shppath,ShapeFileUtil.DEF_ENCODE)//或者直接utf-8
-    val list:List[Any]=Feature.get(shpfile,"PURCHASE")
+    val shpfile: RDD[(String, (Geometry, Map[String, Any]))] = ShapeFileUtil.readShp(sc, shppath, ShapeFileUtil.DEF_ENCODE)
+    //或者直接utf-8
+    val list: List[Any] = Feature.get(shpfile, "PURCHASE")
     //数据类型转换
-    val list_double:List[Double]=list.collect({ case (i: String) => (i.toDouble) })
-    val list_rdd:RDD[Double]=sc.makeRDD(list_double)
+    val list_double: List[Double] = list.collect({ case (i: String) => (i.toDouble) })
+    val list_rdd: RDD[Double] = sc.makeRDD(list_double)
 
-    describe(list_rdd,list_double,10)
+    describe(list_rdd, list_double, 10)
   }
-
-//  //kde
-//  def main(args: Array[String]):Unit= {
-//    val extent=Extent(-109,37,-102,41)
-//
-//    def randomPointFeature(extent: Extent):PointFeature[Double]={
-//      def randInRange(low:Double,high:Double):Double={
-//        val x=Random.nextDouble
-//        low*(1-x)+high*x
-//      }
-//      Feature(Point(randInRange(extent.xmin,extent.xmax),
-//        randInRange(extent.ymin,extent.ymax)),
-//        Random.nextInt%16+16)
-//    }
-//    val pts=(for(i <- 1 to 1000) yield randomPointFeature(extent)).toList
-//    //println(pts)
-//
-//    val kernelWidth:Int=99
-//    val kern:Kernel=Kernel.gaussian(kernelWidth,20,50)
-//    val kde:Tile=pts.kernelDensity(kern,RasterExtent(extent,700,400))
-//
-//    val colorMap=ColorMap(
-//      (0 to kde.findMinMax._2 by 4).toArray,
-//      ColorRamps.HeatmapBlueToYellowToRedSpectrum
-//    )
-//    kde.renderPng(colorMap).write("test.png")
-//  }
 }
