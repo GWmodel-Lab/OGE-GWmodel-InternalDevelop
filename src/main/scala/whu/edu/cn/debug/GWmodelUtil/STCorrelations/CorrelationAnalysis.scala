109f82b86629d3317fc41535b516e8705193ade3
GuoYu <2262704013@qq.com>
2023-08-30 11:41:12 +0800

    STCorrelation add CorrelationAnalysis;CorrelationAnalysis add corr2list&testcorr

--- src/main/scala/whu/edu/cn/debug/GWmodelUtil/STCorrelations/CorrelationAnalysis.scala
+++ src/main/scala/whu/edu/cn/debug/GWmodelUtil/STCorrelations/CorrelationAnalysis.scala
@@ -3,7 +3,11 @@ package whu.edu.cn.debug.GWmodelUtil.STCorrelations
 import org.apache.spark.mllib.stat.Statistics
 import org.apache.spark.rdd.RDD
 import org.locationtech.jts.geom.Geometry
-
+import org.apache.spark.rdd.RDD
+import org.locationtech.jts.geom.{Coordinate, Geometry}
+import whu.edu.cn.oge.Feature
+import scala.collection.mutable.{ArrayBuffer, Map}
+import scala.math.{abs, max, min, pow, sqrt}
 import scala.collection.mutable.Map
 
 object CorrelationAnalysis {
@@ -28,7 +32,7 @@ object CorrelationAnalysis {
     for (i <- 0 to (num - 1)) {
       for (j <- 0 to (num - 1)) {
         cArrSpearman(i)(j) = corr2ml(testshp,pArr(i),pArr(j),"spearman")
-        cArrPearson(i)(j) = corr2ml(testshp,pArr(i),pArr(j),"pearson")
+        cArrPearson(i)(j) = testcorr(testshp,pArr(i),pArr(j))
       }
     }
     println("Spearman correlation")
@@ -60,7 +64,44 @@ object CorrelationAnalysis {
     val time0: Long = System.currentTimeMillis()
     val aX: RDD[Double] = feat.map(t => t._2._2(property1).asInstanceOf[String].toDouble)
     val aY: RDD[Double] = feat.map(t => t._2._2(property2).asInstanceOf[String].toDouble)
-    val correlation: Double = Statistics.corr(aX, aY, method) //"spearman"/"pearson"
+    val correlation: Double = Statistics.corr(aX, aY, method) //"spearman"
+    correlation
+  }
+
+  /**
+   * 输入RDD，求解得到属性p1和p2之间的pearson相关性
+   *
+   * @param testshp RDD[(String, (Geometry, Map[String, Any]))]的形式
+   * @param p1      String的形式
+   * @param p2      String的形式
+   * @return Double 结果corr为两个属性间的相关性
+   */
+  def testcorr(testshp: RDD[(String, (Geometry, Map[String, Any]))], p1: String, p2: String): Double = {
+    val list1: List[Any] = Feature.get(testshp, p1)
+    val list2: List[Any] = Feature.getNumber(testshp, p2)
+    val lst1: List[Double] = list1.collect({ case (i: String) => (i.toDouble) })
+    val lst2: List[Double] = list2.asInstanceOf[List[Double]]
+    val corr = corr2list(lst1, lst2)
+    corr
+  }
+
+  /**
+   * 对lst1和lst2两组数据进行求解得到之间的pearson相关性
+   *
+   * @param lst1 : List[Double]的形式
+   * @param lst2 : List[Double]的形式
+   * @return Double 结果correlation为两组数据之间的相关性
+   */
+  def corr2list(lst1: List[Double], lst2: List[Double]): Double = {
+    val sum1 = lst1.sum
+    val sum2 = lst2.sum
+    val square_sum1 = lst1.map(x => x * x).sum
+    val square_sum2 = lst2.map(x => x * x).sum
+    val zlst = lst1.zip(lst2)
+    val product = zlst.map(x => x._1 * x._2).sum
+    val numerator = product - (sum1 * sum2 / lst1.length)
+    val dominator = pow((square_sum1 - pow(sum1, 2) / lst1.length) * (square_sum2 - pow(sum2, 2) / lst2.length), 0.5)
+    val correlation = numerator / (dominator * 1.0)
     correlation
   }
 }
