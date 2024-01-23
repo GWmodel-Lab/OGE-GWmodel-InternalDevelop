611d3b7050f5a6132896de0a026e698cb86b47cc
OuGuangyu <g_ruoyu@foxmail.com>
2024-01-23 22:26:40 +0800

    add gwda: group data by levels function

---
+++ src/main/scala/whu/edu/cn/algorithms/SpatialStats/GWModels/GWDA.scala
@@ -0,0 +1,126 @@
+package whu.edu.cn.algorithms.SpatialStats.GWModels
+
+import breeze.linalg.{DenseMatrix, DenseVector}
+
+import scala.collection.mutable.ArrayBuffer
+
+class GWDA extends GWRbase {
+
+  var _distinctLevels: Array[_ <: (Any, Int)]= _
+  var _levels: Array[Int] = _
+  var _dataLevels: Array[(String,Int,Int)] = _ //Array是 键，键对应的int值（从0开始计数），对应的索引位置
+  var _dataGroups: Map[String, Array[(String, Int, Int)]] = _
+  var _groupX:ArrayBuffer[DenseMatrix[Double]] = ArrayBuffer.empty[DenseMatrix[Double]]
+  var spweight_groups: ArrayBuffer[Array[DenseVector[Double]]] = ArrayBuffer.empty[Array[DenseVector[Double]]]
+
+  def wqdaCr(bw:Double, kernel: String = "gaussian", adaptive: Boolean = true)={
+    setweight(bw, kernel, adaptive)
+//    val spweightIdx=spweight_dvec.zipWithIndex
+//    spweightIdx.foreach(t=> {
+//      spweight_dvec.foreach(w=>w(t._2)=0)
+//    })
+    for(i<-0 to _xrows){
+      spweight_dvec.map(t=>t(i)=0)
+    }
+    spweight_dvec.foreach(t=>println(t))
+  }
+
+  def getLevels(data: Array[_ <: Any]): Array[Int]={
+    val data_idx=data.zipWithIndex
+    val distin_idx=data.distinct.zipWithIndex
+    val dlevel=data_idx.map(t=>{
+      distin_idx.find(_._1 == data(t._2))
+    })
+//    println(dlevel.toVector)
+    val levels=dlevel.map({
+      getSomeInt(_)
+    })
+//    println(levels.toVector)
+    _distinctLevels=distin_idx
+    _levels=levels
+    _dataLevels=dlevel.zipWithIndex.map(t=>((getSomeStr(t._1),getSomeInt(t._1),t._2)))
+//    println(_dataLevels.toVector)
+    _dataGroups=_dataLevels.groupBy(_._1)
+//    _dataGroups.foreach(println(_))
+//    for(i<-_dataGroups.values){
+//      println(i.toVector)
+//    }
+//    val xizang=_dataGroups("西藏自治区")
+//    println(xizang.toVector)
+    levels
+  }
+
+  def getSomeInt(x: Option[(Any, Int)]): Int = x match {
+    case Some(s) => s._2
+    case None => -1
+  }
+
+  def getSomeStr(x: Option[(Any, Int)]): String = x match {
+    case Some(s) => s._1.toString
+    case None => null
+  }
+
+//  def getSomeAll(x: Option[(Any, Int)]): (String,Int) = x match {
+//    case Some(s) => (s._1.toString,s._2)
+//    case None => (null,-1)
+//  }
+
+  def valSplit(x:Array[DenseVector[Double]] =_X)= {
+    val nlevel = _distinctLevels.length
+    val arrbuf = ArrayBuffer.empty[Double]
+    val weightbuf = ArrayBuffer.empty[DenseVector[Double]]
+    for (i <- 0 until nlevel) { //i 是 第几类的类别循环，有几类循环几次
+      for (groups <- _dataGroups.values) { //groups里面是每一组的情况，已经区分好了的
+        for (j <- x.indices) { // j x的循环，x有多少列，循环多少次
+          groups.foreach(t => {
+            if (t._2 == i) {
+              val tmp = x(j)(t._3)
+              arrbuf += tmp
+            }
+          })
+        }
+
+        groups.foreach(t=>{
+          if (t._2 == i) {
+            weightbuf += spweight_dvec(t._3)//方向错了，按行的，所以要在里面挑
+          }
+        })
+        spweight_groups += weightbuf.toArray
+        weightbuf.clear()
+      }
+      println(arrbuf.toVector)
+      val arrtmp = arrbuf.toArray
+      //      val tmpmat=DenseMatrix.create(rows = arrtmp.length / _xcols ,cols = _xcols, data = arrtmp)
+      //      println(tmpmat)
+      _groupX += DenseMatrix.create(rows = arrtmp.length / _xcols, cols = _xcols, data = arrtmp)
+      //      _groupX += tmpmat
+      arrbuf.clear()
+
+    }
+    _groupX.foreach(println(_))
+    spweight_groups.foreach(t=>t.foreach(println(_)))
+  }
+
+  def selectVal(x:Array[DenseVector[Double]] =_X, idx:Int,row:Int,col:Int)={
+    val mat= DenseMatrix.zeros[Double](row,col)
+    val arrbuf=new ArrayBuffer[Double]()
+    for(i<-x.indices){
+      arrbuf += x(i)(idx)
+    }
+
+  }
+
+
+
+  def wqda()={
+
+  }
+
+  def wlda(x:Array[DenseVector[Double]], grouping: Array[Int], xpr:Array[DenseVector[Double]], w:  Array[DenseVector[Double]]=spweight_dvec)={
+    val nlevel=_distinctLevels.length
+
+
+  }
+
+
+}
--- src/main/scala/whu/edu/cn/algorithms/SpatialStats/Test/test.scala
+++ src/main/scala/whu/edu/cn/algorithms/SpatialStats/Test/test.scala
@@ -1,6 +1,6 @@
 package whu.edu.cn.algorithms.SpatialStats.Test
 
-import breeze.linalg.DenseVector
+import breeze.linalg.{DenseMatrix, DenseVector}
 import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 import whu.edu.cn.algorithms.SpatialStats.BasicStatistics.{AverageNearestNeighbor, DescriptiveStatistics}
@@ -12,6 +12,7 @@ import whu.edu.cn.algorithms.SpatialStats.SpatialRegression.{SpatialDurbinModel,
 import whu.edu.cn.algorithms.SpatialStats.Utils.FeatureDistance._
 import whu.edu.cn.algorithms.SpatialStats.Utils.OtherUtils._
 import whu.edu.cn.algorithms.SpatialStats.GWModels.GWRbasic
+import whu.edu.cn.algorithms.SpatialStats.GWModels.GWDA
 import whu.edu.cn.algorithms.SpatialStats.GWModels.GWAverage
 import whu.edu.cn.algorithms.SpatialStats.GWModels.GWCorrelation
 import whu.edu.cn.algorithms.SpatialStats.STCorrelations.{CorrelationAnalysis, SpatialAutoCorrelation, TemporalAutoCorrelation}
@@ -23,7 +24,7 @@ import whu.edu.cn.util.ShapeFileUtil._
 object test {
   //global variables
   val conf: SparkConf = new SparkConf().setMaster("local[8]").setAppName("query")
-      .set("spark.testing.memory", "512000000")
+//      .set("spark.testing.memory", "512000000")
   val sc = new SparkContext(conf)
   val encode="utf-8"
 
@@ -34,6 +35,7 @@ object test {
   val shpfile2 = readShp(sc, shpPath2, encode)
 
   val shpPath3: String = "src\\main\\scala\\whu\\edu\\cn\\algorithms\\SpatialStats\\Test\\testdata\\LNHP100.shp"
+//  val shpPath3="D:\\Java\\testdata\\USelect2004.shp"
   val shpfile3 = readShp(sc, shpPath3, encode)
 
   val csvpath = "src\\main\\scala\\whu\\edu\\cn\\algorithms\\SpatialStats\\Test\\testdata\\test_aqi.csv"
@@ -46,6 +48,30 @@ object test {
     //    acf_test()
     //    linear_test()
     //    pca_test()
+
+    val m=new GWDA
+    m.init(shpfile3)
+    m.setX("PURCHASE,FLOORSZ,PROF")
+    m.setY("TYPEDETCH")
+    m.setweight(50,"bisquare",true)
+    val tmp=DenseVector(shpfile3.map(t => t._2._2("TYPEDETCH")).collect())
+    m.getLevels(tmp.toArray)
+    m.valSplit()
+
+    val arr=Array(DenseVector(1.0,2,3),DenseVector(4.0,5,6))
+    arr.foreach(println(_))
+    val arr2 =arr.flatMap(t => t.toArray)
+    val mat=DenseMatrix.create(2,3,arr2)
+    println(mat)
+    println(arr2.toVector)
+    val mat2=mat.t
+    println(mat2)
+    val arr3=arr2.zipWithIndex
+    val arr4=arr3.groupBy(t=>{t._2 % 2}).toArray
+    val arr5=arr4.map(t=>t._2.map(_._1))
+    arr5.foreach(t=>println(t.toVector))
+    val mat3=DenseMatrix.create(3,2,arr5.flatten)
+    println(mat3)
 
     //    GWCorrelation.cal(sc, shpfile, "aging", "GDP,pop", bw=20, kernel = "bisquare", adaptive = true)
     //    GWAverage.cal(sc, shpfile, "aging", "GDP,pop", 50)
@@ -70,7 +96,7 @@ object test {
     //    println(Geodetector.factorDetector(shpfile, "aging", "PCGDP,GI,FD,education,GDP,province,SIP,TIP,PIP,pop,city,employee"))
     //    println(Geodetector.interactionDetector(shpfile, "aging", "PCGDP,GI,FD,education,GDP,province,SIP,TIP,PIP,pop,city,employee"))
     //    println(Geodetector.ecologicalDetector(shpfile, "aging", "PCGDP,GI,FD,education,GDP,province,SIP,TIP,PIP,pop,city,employee"))
-        println(Geodetector.riskDetector(shpfile, "aging", "PCGDP,GI,FD,education,GDP,province,SIP,TIP,PIP,pop,city,employee"))
+//        println(Geodetector.riskDetector(shpfile, "aging", "PCGDP,GI,FD,education,GDP,province,SIP,TIP,PIP,pop,city,employee"))
     //    println(Geodetector.riskDetector(shpfile, "GDP", "province"))
     //    println(Geodetector.interactionDetector(shpfile2, "HR60", "PO60,DV60,STATE_NAME"))
     //    val rddSample=SandwichSampling.sampling(sc, shpfile3,"PURCHASE", "FLOORSZ", "TYPEDETCH")
